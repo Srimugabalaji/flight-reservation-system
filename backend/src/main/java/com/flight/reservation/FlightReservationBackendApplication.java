0c211678c567afe44e1c222d4e4a8373bdcb85de
Srimugabalaji03 <srimugablaji03@gmail.com>
2026-09-24 07:11:08 +0000

    Initial project structure

---
+++ backend/src/main/java/com/flight/reservation/FlightReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flight.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightReservationBackendApplication.class, args);
+	}
+
+}
